3f4715c8f3beae42b44232c5163f6417700098d5
Garrett Swann <Garrettswann3049@yahoo.com>
2016-09-26 11:52:08 -0700

    Added many test cases to check Mine-Game. Also modified Makefile so it works on my system.

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,62 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+TEST(FieldTest, placeLimit0_0)
+{
+	Field minefield;
+	minefield.placeMine(0,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(0,0) );
+}
+TEST(FieldTest, placeLimit0_10)
+{
+	Field minefield;
+	minefield.placeMine(0,10);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(0,10) );
+}
+TEST(FieldTest, placeLimit10_0)
+{
+	Field minefield;
+	minefield.placeMine(10,0);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(10,0) );
+}
+TEST(FieldTest, placeLimit10_10)
+{
+	Field minefield;
+	minefield.placeMine(10,10);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(10,10) );
+}
+
+
+/*  void function lol
+TEST(FieldTest, placeMineOutBounds)
+{
+    Field minefield;
+
+    ASSERT_ANY_THROW(minefield.placeMine(-1,-1));
+}
+*/
+
+TEST(FieldTest, getOutBounds)
+{
+    Field minefield;
+    ASSERT_ANY_THROW(minefield.get(-1,-1));
+    ASSERT_ANY_THROW(minefield.get(-1,11));
+    ASSERT_ANY_THROW(minefield.get(11,11));
+    ASSERT_ANY_THROW(minefield.get(11,-1));
+}
+
+TEST(FieldTest, getUnplaced)
+{
+    Field mf;
+    ASSERT_EQ(EMPTY_HIDDEN, mf.get(5,5));
+}
+
+TEST(FIELDTEST, safeOutBounds)
+{
+    Field mf;
+    ASSERT_FALSE(mf.isSafe(-1, 5));
+    ASSERT_FALSE(mf.isSafe(-1, 11));
+    ASSERT_FALSE(mf.isSafe(11,5));
+    ASSERT_FALSE(mf.isSafe(11,-1));
+}
+
